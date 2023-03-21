fc46876edffa08956ee833fa9c9c419522a0dc78
Shubham Waghule <shubhamwaghule.0777@gmail.com>
2023-03-21 11:08:03 +0530

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Shubham Waghule`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p> I am constantly striving to enhance my knowledge and skills in web development and problem-solving, while also delving into the exciting and rapidly evolving fields of blockchain and artificial intelligence.</p>
+                  <p>I am a Computer Engineering student currently pursuing my B.Tech degree at Vishwakarma Institute of Technology in Pune. I am constantly striving to enhance my knowledge and skills in web development and problem-solving, while also delving into the exciting and rapidly evolving fields of blockchain and artificial intelligence.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
